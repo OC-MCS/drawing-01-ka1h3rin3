1b6fb76f1e3aff1e3a89be356dee0eac4cb09ce5
Katherine Reed <katherinereed992@gmail.com>
2019-03-19 21:54:04 -0500

    Added Shape.cpp

--- Drawing/DrawingUI.h
+++ Drawing/DrawingUI.h
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace sf;
 #include "ShapeMgr.h"
+#include "Shape.h"
 
 // finish this code; add functions, data as needed
 
@@ -15,9 +16,32 @@ public:
 	DrawingUI(Vector2f p)
 	{
 	}
-
+	void die(string msg)
+	{
+		cout << msg << endl;
+		exit(-1);
+	}
 	void draw(RenderWindow& win, ShapeMgr *mgr)
 	{
+		
+		
+		DrawingShape* c1 = new Circle(15, 30, 12, Color::Red);
+		DrawingShape* s1 = new Square(50, 75, 12, Color::Blue);
+		c1->draw(win);
+		s1->draw(win);
+		
+
+
+		Font font;
+		if (!font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf"))
+			die("couldn't load font");
+
+		// title
+		Text title("Drawing Color", font, 25);
+		title.setPosition(30, 25);
+		win.draw(title);
+
+
 	}
 	
 	bool isMouseInCanvas(Vector2f mousePos)
--- Drawing/Shape.h
+++ Drawing/Shape.h
@@ -10,9 +10,31 @@ using namespace sf;
 // for Circle and Square
 class DrawingShape 
 {
+protected:
+	int mX, mY, mSize;
+	Color mColor;
+	bool mFilled;
+public:
+	void setFilled(bool filled);
+	DrawingShape(int x, int y, int size, Color color_shade);
+	virtual void draw(RenderWindow& win) = 0;
 };
 
 // add Circle, Square classes below. These are derived from DrawingShape
+
+class Circle : public DrawingShape
+{
+public:
+	Circle(int x, int y, int radius, Color color);
+	virtual void draw(RenderWindow& win);
+};
+
+class Square : public DrawingShape
+{
+public:
+	Square(int x, int y, int width, Color color);
+	virtual void draw(RenderWindow& win);
+};
 
 
 
